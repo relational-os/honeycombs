9b862e65f8b1162fb8ff1e5a65a2b2141d5036f4
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-07-13 19:31:23 -0500

    Add ipfs publish flow

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,10 +1,40 @@
 import React, {useState} from 'react';
-
+    
 const WritePage = () => {
   const [text, setText] = useState("");
+  const [blockHash, setBlockHash] = useState(null);
 
-  const onSubmit = () => {
-    // console.log('our submitted text is:', text);
+  const onSubmit = async () => {
+    console.log('our submitted text is:', text);
+
+    let targetBlock = {
+        "text": text
+        // "author": ""
+    }
+
+    try {
+        let uploadURL = `${process.env.NEXT_PUBLIC_IPFS_TARGET}/uploadJSON`
+        console.log('pushing to', uploadURL);
+
+        // upload to our IPFS bridge
+        const bridgeResponse = await fetch(
+            uploadURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(targetBlock)
+        }).then((res) => {
+            return res.json();
+        });
+
+        console.log('brdigeResponse', bridgeResponse);
+        setBlockHash(bridgeResponse.hash);
+    } catch (error) {
+        console.log('error', error);
+    }
+    
+    // on success
     setText("");
   }
 
@@ -12,16 +42,38 @@ const WritePage = () => {
     <div className="frame">
       <h1>Write</h1>
 
-      <input value={text} onChange={event => setText(event.target.value)} className="write-input"></input>
+      <input 
+        value={text} 
+        onChange={event => setText(event.target.value)} 
+        className="write-input">
+      </input>
+
       {text.trim().length > 0 && 
-        <button onClick={onSubmit}>send</button>
+        <button className="post-button" onClick={onSubmit}>post</button>
       }
+
+      <div className="block-target">
+        {!!blockHash &&
+          <a 
+            target="_blank" 
+            href={`https://ipfs.io/ipfs/${blockHash}`}>
+                published block
+          </a>
+        }
+      </div>
+
       <style jsx>{`
         .frame {
           padding: 20px;
         }
         .write-input {
             background-color: darkgray;
+        }
+        .block-target {
+            margin-top: 1rem;
+        }
+        .post-button {
+            margin-left: .5rem;
         }
       `}</style>
     </div>
