055b64b72e269dde68f80e5cf674b0e8b4bd3950
CJ Pais <cj@cjpais.com>
2021-08-23 15:34:30 -0700

    query all users instead of just self

--- src/app/components/queryBar.tsx
+++ src/app/components/queryBar.tsx
@@ -22,6 +22,25 @@ const useQuery = (
   return { data, error, refresh: mutate };
 };
 
+const baseQuery = gql`
+  query BlockQuery($author: String) {
+    blocks(
+      first: 25
+      orderBy: datetime
+      orderDirection: desc # where: { author: $author }
+    ) {
+      id
+      owner
+      author
+      content
+      context
+      uri
+      type
+      datetimeString
+    }
+  }
+`;
+
 const QueryBar = () => {
   const [query, setQuery] = useState("");
   const [params, setParams] = useState<Record<string, string>>({});
@@ -30,28 +49,10 @@ const QueryBar = () => {
   const { account } = useWallet();
 
   useEffect(() => {
+    console.log(account);
     if (account !== undefined) {
       setParams({ author: account });
-
-      setGQLQuery(gql`
-        query BlockQuery($author: String) {
-          blocks(
-            first: 10
-            orderBy: datetime
-            orderDirection: desc
-            where: { author: $author }
-          ) {
-            id
-            owner
-            author
-            content
-            context
-            uri
-            type
-            datetimeString
-          }
-        }
-      `);
+      setGQLQuery(baseQuery);
     }
   }, [account]);
 
